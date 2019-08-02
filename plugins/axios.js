cf8db5fd507483890d279be023d4ad3e6c721670
satou321 <iitsukanan@yahoo.co.jp>
2019-08-02 23:48:22 +0900

    cors

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,20 +1,35 @@
 import Vue from 'vue';
 
-export default function ({$axios}) {
+export default function({ $axios }) {
   $axios.onRequest(config => {
-
-    const localAddr = "http://" + process.env.HOST + ":" + process.env.PORT + "/";
+    const localAddr =
+      'http://' + process.env.HOST + ':' + process.env.PORT + '/';
     // return console.log(localAddr,$axios.defaults.baseURL);
     if (localAddr === $axios.defaults.baseURL) {
-      return Promise.reject("[axios.js]local address same as the $axios base URL => local:" + localAddr + " axios:" + $axios.defaults.baseURL);
-    } else if (localAddr === undefined || $axios.defaults.baseURL === undefined) {
-      return Promise.reject("[axios.js]local address or $axios base URL is undefined=> local:" + localAddr + " axios:" + $axios.defaults.baseURL);
+      return Promise.reject(
+        '[axios.js]local address same as the $axios base URL => local:' +
+          localAddr +
+          ' axios:' +
+          $axios.defaults.baseURL
+      );
+    } else if (
+      localAddr === undefined ||
+      $axios.defaults.baseURL === undefined
+    ) {
+      return Promise.reject(
+        '[axios.js]local address or $axios base URL is undefined=> local:' +
+          localAddr +
+          ' axios:' +
+          $axios.defaults.baseURL
+      );
     }
 
-    config.headers.common['Content-Type'] = 'application/json'
+    config.headers.common['Content-Type'] = 'application/json';
 
     // config.headers.common['Authorization'] = process.env.APIKEY;
     // console.log(3333, config.headers.common);
+
+    return config;
   });
   $axios.onError(e => {
     if (process.browser) {
