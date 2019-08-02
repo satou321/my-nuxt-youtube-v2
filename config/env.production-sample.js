7e395411f5105df261a10f4059b429b1fb6430b4
satou321 <iitsukanan@yahoo.co.jp>
2019-08-02 18:57:34 +0900

    tweak

--- config/env.production-sample.js
+++ config/env.production-sample.js
@@ -2,12 +2,12 @@
 * 非公開変数はここに書かずに直接サーバーで設定してください
 * */
 module.exports = {
-  APIKEY: 'AIzaSyDbALNJ0CF2YS5EN9sApUOkulyY8yd7Hw',
-  AUTHDOMAIN: 'vuestock-c22b8.firebaseapp.com',
-  DATABASEURL: 'https://vuestock-c22b8.firebaseio.com',
-  PROJECTID: 'vuestock-c22b8',
+  APIKEY: '',
+  AUTHDOMAIN: '',
+  DATABASEURL: '',
+  PROJECTID: '',
   STORAGEBUCKET: '',
-  MESSAGINGSENDERID: '164322585546',
-  APPID: '1:164322585546:web:6b391f62d4443573',
-  VIDEO_API_URL: 'https://rocky-falls-88494.herokuapp.com/',
+  MESSAGINGSENDERID: '',
+  APPID: '',
+  VIDEO_API_URL: '',
 };
--- config/env.stage-sample.js
+++ config/env.stage-sample.js
@@ -3,16 +3,15 @@
 * 非公開変数を記述していいですが、.gitignoreしてください。
 * */
 module.exports = {
-  APIKEY: 'AIzaSyDbALNJ0CF2YS5EN9sApUO_kulyY8yd7Hwa',
-  AUTHDOMAIN: 'vuestock-c22b8.firebaseapp.com',
-  DATABASEURL: 'https://vuestock-c22b8.firebaseio.com',
-  PROJECTID: 'vuestock-c22b8',
+  APIKEY: '',
+  AUTHDOMAIN: '',
+  DATABASEURL: '',
+  PROJECTID: '',
   STORAGEBUCKET: '',
-  MESSAGINGSENDERID: '164322585546',
-  APPID: '1:164322585546:web:6b391f62d4443573',
-  // VIDEO_API_URL: 'http://10.0.1.6:8082',
-  VIDEO_API_URL: 'https://rocky-falls-88494.herokuapp.com/',
-  HOST: '10.0.1.6',
-  PORT: '3002',
-  //SECRET: '32i491hoinoapjkn@ld[@i113o2n',
+  MESSAGINGSENDERID: '',
+  APPID: '',
+  VIDEO_API_URL: '',
+  HOST: '',
+  PORT: '',
+  //SECRET: '',
 };
